4b89161ac4d0c1d66007ccdc5b72f843f53e02ac
Alan Charles <alanjcharles14@gmail.com>
2018-05-08 19:59:50 -0600

    Add scroll effect for crawl text

--- src/CrawlText/CrawlText.js
+++ src/CrawlText/CrawlText.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './crawlText.css'
 
 export const CrawlText = ({data}) => {
     return (
-        <div>
-         <h3>{data.crawlText}</h3>
-         <h5>{data.title}</h5>
-         <h5>{data.release}</h5>   
-        </div>
+        <section className="crawl-section">
+            <div className="crawl-div">
+                <p>{data.crawlText}</p>
+                <div className="title">
+                    <h5>{data.title}</h5>
+                    <h5>{data.release}</h5>   
+                </div>
+            </div>
+        </section>
     );
 }
+
