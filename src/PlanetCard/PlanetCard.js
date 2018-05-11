d01929baa4687dfff5e1be69b882eb50c635a858
Alan Charles <alanjcharles14@gmail.com>
2018-05-11 08:50:06 -0600

    Add planet card styling

--- src/PlanetCard/PlanetCard.js
+++ src/PlanetCard/PlanetCard.js
@@ -1,45 +1,36 @@
 import React from 'react'
+import './PlanetCard.css'
 
 const PlanetCard = (props) => {
     const{name, terrain, population, climate, residents} = props
 
      return (
          <article className="planet-card">
-            <section className="name-section">
+            <section className="planet-name-section">
                 <h1 className="planet-name">{name}</h1>
             </section>
-            <div>
-                <p className="description-title">
-                <h3>terrain: {terrain}</h3>
+            <div className="content-div">
+                <p className="description-title">Terrain</p>
+                <h3 className="description-content">{terrain}</h3>
             </div>
-         <h3>population: {population}</h3>
-         <h3>climate: {climate}</h3>
-         <h3>residents: {residents}</h3>
+            <hr></hr>
+            <div className="content-div">
+                <p className="description-title">Population</p>
+                <h3 className="description-content">{population}</h3>
+            </div>
+            <hr></hr>
+            <div className="content-div">
+                <p className="description-title">Climate</p>
+                <h3 className="description-content">{climate}</h3>
+            </div>
+            <hr></hr>
+            <div className="content-div">
+                <p className="description-title">Residents</p>
+                <h3 className="description-content residents">{residents}</h3>
+            </div>
          </article>
      )
      
  }
  
  export default PlanetCard
-
-
-//  <article className="person-card">
-//  <section className="name-section">
-//      <h1 className="person-name">{name}</h1>
-//  </section>
-//  <div className="content-div">
-//      <p className="description-title">Origin</p>
-//      <h3 className="description-content">{planet}</h3>
-//  </div>
-//  <hr></hr>
-//  <div className="content-div">
-//      <p className="description-title">Species</p>
-//      <h3 className="description-content">{species}</h3>
-//  </div>
-//  <hr></hr>
-//  <div className="content-div">
-//      <p className="description-title">Population</p>
-//      <h3 className="description-content">{population}</h3>
-//  </div>
-// </article>
-// )
